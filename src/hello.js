4f128fd715c6a1ea37f3b06b7a783438a6eacdfb
Prerna Kashyap <pk2600@columbia.edu>
2018-11-11 14:17:03 -0500

    more hw2 updates

--- src/hello.js
+++ src/hello.js
@@ -3,6 +3,7 @@ const REGION = "us-west-2";
 const USER_POOL_ID = 'us-west-2_0pbBNuSKk';
 const CLIENT_ID = '3d5ptsm32jnpln89d2rs4p7hsj';
 const IDENTITY_POOL_ID = 'us-west-2:4b6b2e57-e660-466a-9800-7b2206ddce1c';
+const authenticator = `cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}`;
 var AWS = require('aws-sdk');
 
 var poolData = {
@@ -10,64 +11,67 @@ var poolData = {
   ClientId : CLIENT_ID
 };
 
+var authenticationData = {
+  Username : 'pk2600@columbia.edu',
+  Password : 'Qwerty123#',
+};
+
+var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+  Username : 'pk2600@columbia.edu',
+  Pool : userPool
+};
+
 var messages = [],
 lastUserMessage = "",
 botMessage = "Kucch toh bolo",
 botName = 'Chatbot';
 var loggedIn = false;
-var config = {};
+var config = null;
 var apigClientFactory = require('aws-api-gateway-client').default;
 var apigClient = null;
 
-function login() {
-    var authenticationData = {
-    Username : 'pk2600@columbia.edu',
-    Password : 'Qwerty123#',
-  };
-  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  var userData = {
-    Username : 'pk2600@columbia.edu',
-    Pool : userPool
-  };
+function login(callback) {
   var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
         console.log(result);
-        var accessToken = result.getAccessToken().getJwtToken();
+        //var accessToken = result.getAccessToken().getJwtToken();
         var idToken = result.getIdToken().getJwtToken();
-
-        //POTENTIAL: Region needs to be set if not already set previously elsewhere.
         AWS.config.region = REGION;
         AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId : IDENTITY_POOL_ID, // your identity pool id here
+            IdentityPoolId : IDENTITY_POOL_ID,
             Logins : {
-                // Change the key below according to the specific region your user pool is in.
-                'cognito-idp.us-west-2.amazonaws.com/us-west-2_0pbBNuSKk' : idToken
+                [authenticator] : idToken
             }
         });
-
-        //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
         AWS.config.credentials.refresh((error) => {
             if (error) {
-                 console.error(error);
+                console.error(error);
             } else {
-                 // Instantiate aws sdk service objects now that the credentials have been updated.
-                 // example: var s3 = new AWS.S3();
-                  console.log('Successfully logged!');
-                  loggedIn = true;
+                console.log('Successfully logged!');
+                loggedIn = true;
+                config = {
+                  apiKey : 'Aw6Pqn4Ef51vqKQg1nxgn6kdKU5ijBNuBxqPLL91',
+                  invokeUrl : 'https://bt3rsxq007.execute-api.us-west-2.amazonaws.com/test',
+                  region : REGION,
+                  accessKey : AWS.config.credentials.accessKeyId, // REQUIRED
+                  secretKey : AWS.config.credentials.secretAccessKey, // REQUIRED
+                  sessionToken : AWS.config.credentials.sessionToken
+                };
+                callback(config);
             }
         });
     },
     onFailure: function(err) {
-        alert(err.message || JSON.stringify(err));
+      alert(err.message || JSON.stringify(err));
+      callback(null);
     },
-
   });
 }
 
-function getCurrentUser() {
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+function getCurrentUser(callback) {
     var cognitoUser = userPool.getCurrentUser();
     if (cognitoUser != null) {
         cognitoUser.getSession(function(err, session) {
@@ -79,6 +83,8 @@ function getCurrentUser() {
             cognitoUser.getUserAttributes(function(err, attributes) {
                 if (err) {
                     console.log('unable to get user attributes');
+                    alert(err.message || JSON.stringify(err));
+                    return;
                 } else {
                     console.log('got user attributes');
                 }
@@ -86,7 +92,7 @@ function getCurrentUser() {
             AWS.config.credentials = new AWS.CognitoIdentityCredentials({
                 IdentityPoolId : IDENTITY_POOL_ID,
                 Logins : {
-                    'cognito-idp.us-west-2.amazonaws.com/us-west-2_0pbBNuSKk' : session.getIdToken().getJwtToken()
+                    [authenticator] : session.getIdToken().getJwtToken()
                 }
             });
             config = {
@@ -97,48 +103,65 @@ function getCurrentUser() {
               secretKey : AWS.config.credentials.secretAccessKey, // REQUIRED
               sessionToken : AWS.config.credentials.sessionToken
             };
-            apigClient = apigClientFactory.newClient(config);
         });
+    }
+    callback(config);
+}
+
+function chatbotResponseUtil() {
+  apigClient = apigClientFactory.newClient(config);
+   if (document.getElementById("chatbox").value !== "") {
+      lastUserMessage = document.getElementById("chatbox").value;
+      messages.push(lastUserMessage);
+      document.getElementById("chatbox").value = "";
+      var params = {};
+      var additionalParams = {
+        headers: {
+          "Access-Control-Allow-Origin" : "*",
+          "Access-Control-Allow-Credentials" : true,
+          "Access-Control-Allow-Methods" : "'GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+          "Content-Type" : "application/json",
+        },
+        queryParams: {}
+      };
+      var body = {
+        "question" : lastUserMessage,
+      };
+      apigClient.invokeApi(params, '/chatbot', 'POST', additionalParams, body).then(function(result) {
+        console.log("Sucessfully got chatbot response")
+        botMessage = String(result.data.answer);
+        console.log(botMessage);
+        messages.push("<b>" + botName + ":</b> " + botMessage);
+        for (var i = 1; i < 8; i++) {
+          if (messages[messages.length - i])
+            document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
+        }
+      }).catch(function(result) {
+        console.error("Chatbot response failure")
+      });
     }
 }
 
-function chatbotResponse(argument) {
+function chatbotResponse() {
   if(loggedIn === false) {
-    login();
+    login(function(res) {
+        config = res;
+        console.log('Login complete');
+        console.log(config);
+        chatbotResponseUtil();
+    });
   }
-  getCurrentUser();
- if (document.getElementById("chatbox").value !== "") {
-    lastUserMessage = document.getElementById("chatbox").value;
-    messages.push(lastUserMessage);
-    document.getElementById("chatbox").value = "";
-    var params = {};
-    var additionalParams = {
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : true,
-        "Access-Control-Allow-Methods" : "'GET, POST, PATCH, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
-        "Content-Type" : "application/json",
-      },
-      queryParams: {}
-    };
-    var body = {
-      "question" : lastUserMessage,
-    };
-    apigClient.invokeApi(params, '/chatbot', 'POST', additionalParams, body).then(function(result) {
-      console.log("Sucessfully got chatbot response")
-      botMessage = String(result.data.answer);
-      console.log(botMessage);
-      messages.push("<b>" + botName + ":</b> " + botMessage);
-      for (var i = 1; i < 8; i++) {
-        if (messages[messages.length - i])
-          document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
-      }
-    }).catch(function(result) {
-      console.error("Chatbot response failure")
+  else {
+    getCurrentUser(function(res) {
+        config = res;
+        console.log('User verified');
+        console.log(config);
+        chatbotResponseUtil();
     });
   }
 }
+
 //runs the keypress() function when a key is pressed
 document.onkeypress = keyPress;
 //if the key pressed is 'enter' runs the function newEntry()
